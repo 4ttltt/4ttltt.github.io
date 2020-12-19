602b8831362c72f683c267071e07ce5341597dab
uyenzen <tuthanhdelatoithuong@gmail.com>
2020-12-19 16:54:08 +0700

    load videos

---
+++ components/media/index.js
@@ -0,0 +1,94 @@
+/* eslint-disable */
+
+import mediaStyles from "./index.module.css";
+// import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
+import ReactPlayer from "react-player";
+export const mediaType = {
+  fbVideo: "fbVideo",
+  mixCloud: "mixCloud"
+};
+
+export function FbEmbedded({ href }) {
+  // const isMobile = useMediaQuery({ maxWidth: 640 });
+  // const finalWidth = isMobile ? 320 : 560;
+  // const finalHeight = isMobile ? (320 * 314) / 560 : 314;
+  // key={`${href} ${isMobile} ${finalWidth} ${finalHeight}`}
+  return (
+    <div className={mediaStyles.responsivePlayer}>
+      <ReactPlayer
+        width="100%"
+        heigh="100%"
+        className={mediaStyles.reactPlayer}
+        url={href}
+        controls
+      />
+    </div>
+  );
+}
+
+export function FbWrap({ title, href }) {
+  const [showView, setShowVideo] = useState(false);
+  return (
+    <div>
+      <a
+        href="#"
+        role="button"
+        tabIndex={0}
+        aria-hidden="true"
+        className="m-1 text-green-500 border-1"
+        onClick={() => {
+          setShowVideo(true);
+        }}
+      >
+        {" "}
+        Mở/Play{" "}
+      </a>
+      {"  "}
+
+      <a
+        href={href}
+        target="_blank"
+        rel="noreffer noopenner"
+        className="m-1 text-green-500 border-1"
+      >
+        {" "}
+        Mở tại trang khác{" "}
+      </a>
+      {showView ? <FbEmbedded href={href} /> : null}
+    </div>
+  );
+}
+export function MixEmbedded({ href, width = "100%", height = "60" }) {
+  return <iframe width={width} height={height} src={href} frameborder="0" />;
+}
+
+export function MediaItem(props) {
+  const { title, type } = props;
+  return (
+    <div className={mediaStyles.mediaItem}>
+      <div className={mediaStyles.mediaItemTitle}>{title}</div>
+      <div className={mediaStyles.mediaItemContent}>
+        {type === mediaType.fbVideo ? (
+          <FbWrap {...props} />
+        ) : type === mediaType.mixCloud ? (
+          <MixEmbedded {...props} />
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export const MediaCategory = ({ categoryName, items = [] }) => {
+  const renderItem = items => null;
+  return (
+    <div className={mediaStyles.mediaCategory}>
+      <div className={mediaStyles.mediaCategoryHeader}>{categoryName}</div>
+      <div className={mediaStyles.mediaCategoryBody}>
+        {items.map(item => (
+          <MediaItem key={item.href} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
--- pages/dpa.js
+++ pages/dpa.js
@@ -1,12 +1,180 @@
+/* eslint-disable */
 import PropTypes from 'prop-types';
 import StaticLayout from '../components/staticLayout';
 import { getPageContentBySlug } from '../lib/markdown';
 import { getNavigationLinks } from '../lib/navigation';
+import { MediaCategory, mediaType } from '../components/media';
+
+const persons = {
+  hv: 'Hòa Võ',
+  mn: 'Minh Nguyệt',
+  vtb: 'Vũ Thúy Bình',
+  hy: 'Hoàng Yến',
+  hn: 'Hạnh Ngọc',
+  tnh: 'Tâm Như Hạnh',
+  other: '??',
+};
+
+const hvVideos = [
+  {
+    title: 'Bố Thí, Trì Giới, Các cõi trời',
+    type: mediaType.fbVideo,
+    href: 'https://facebook.com/tuthanhde2018/videos/447762136235883/',
+    speaker: persons.hv,
+    author: persons.hv,
+    date: '',
+  },
+  {
+    title: 'Sơ thiện, Trung Thiện, Hậu Thiện',
+    type: mediaType.fbVideo,
+    href: 'https://www.facebook.com/hoa.vo.5855594/videos/2763907837201221/',
+    speaker: persons.hv,
+    author: persons.hv,
+    date: '',
+  },
+  {
+    title:
+      'Khổ Thánh Đế cần phải liễu tri ?Khổ Tập Thánh đế cần phải đoạn tận ? Khổ Diệt Thánh Đế cần phải chứng ngộ ? Khổ Diệt Đạo Thánh Đế cần phải tu tập ?',
+    href: 'https://www.facebook.com/2043864905835119/videos/2680505228832631/',
+  },
+  {
+    title: 'Tứ thánh đế 12 nhân duyên & 8 thánh đạo',
+    href: 'https://www.facebook.com/2043864905835119/videos/874087426400396/',
+  },
+  {
+    title: '44 căn bản trí',
+    href: 'https://www.facebook.com/2043864905835119/videos/156625042155062/',
+  },
+  {
+    title: 'Đoạn tận ái',
+    href: 'https://www.facebook.com/2043864905835119/videos/259062188757424/',
+  },
+  {
+    title: 'Bài 12 tên cướp phần 1',
+    href: 'https://www.facebook.com/2043864905835119/videos/675421196569714/',
+  },
+  {
+    title: 'Bài 12 tên cướp phần 1 và phần 2.',
+    href: 'https://www.facebook.com/2043864905835119/videos/1047202079008859',
+  },
+  {
+    title: 'Bát thánh đạo - Bài Chánh tri kiến và chánh tư duy',
+    href: 'https://www.facebook.com/2043864905835119/videos/367447570888823',
+  },
+  {
+    title: 'Bát thánh đạo - Chánh ngữ/nghiệp/tinh tấn',
+    href: 'https://www.facebook.com/2043864905835119/videos/299095561233049',
+  },
+  {
+    title: 'Bát thánh đạo - Chánh niệm & chánh định',
+    href: 'https://www.facebook.com/2043864905835119/videos/895623750930736',
+  },
+  {
+    title: 'Thiền chỉ - Thiền quán',
+    href: 'https://www.facebook.com/2043864905835119/videos/691156548382001/',
+  },
+].map((x) => ({
+  ...x,
+  type: mediaType.fbVideo,
+  speaker: persons.hv,
+  author: persons.hv,
+  date: '',
+}));
 
 export default function DPA({ page, links }) {
   const {
     title, image, slug, markdown,
   } = page;
+  const data = [
+    {
+      categoryName: 'Video Hoa Vo',
+      items: hvVideos,
+      // items: [
+      //   {
+      //     title: "Bố Thí, Trì Giới, Các cõi trời",
+      //     type: mediaType.fbVideo,
+      //     href:
+      //       "https://facebook.com/tuthanhde2018/videos/447762136235883/",
+      //     speaker: persons.hv,
+      //     author: persons.hv,
+      //     date: ""
+      //   },
+      //   {
+      //     title: "Sơ thiện, Trung Thiện, Hậu Thiện",
+      //     type: mediaType.fbVideo,
+      //     href:
+      //       "https://www.facebook.com/hoa.vo.5855594/videos/2763907837201221/",
+      //     speaker: persons.hv,
+      //     author: persons.hv,
+      //     date: ""
+      //   }
+      // ]
+    },
+    {
+      categoryName: 'Diệu Pháp Âm Sách 4 Thánh Đế',
+      items: [
+        {
+          title: 'Sách Tứ Thánh Đế - Giọng Đọc - Minh Nguyệt',
+          type: mediaType.mixCloud,
+          href:
+            'https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fuy%C3%AAn-nguy%E1%BB%85n122%2Fplaylists%2Fmn-4thanhde-latoithuong%2F',
+          speaker: persons.mn,
+          author: persons.hv,
+          date: '',
+        },
+        {
+          title: 'Sách Tứ Thánh Đế - Giọng Đọc - Hạnh Ngọc',
+          type: mediaType.mixCloud,
+          href:
+            'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fuy%C3%AAn-nguy%E1%BB%85n122%2Fplaylists%2F4thanhde-latoithuong-hanh-ngoc%2F',
+          speaker: persons.hy,
+          author: persons.hv,
+          date: '',
+        },
+        {
+          title: 'Sách Tứ Thánh Đế - Giọng Đọc - Hoàng Yến',
+          type: mediaType.mixCloud,
+          href:
+            'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fuy%C3%AAn-nguy%E1%BB%85n122%2Fplaylists%2Fhy-4thanhde-latoithuong%2F',
+          speaker: persons.hy,
+          author: persons.hv,
+          date: '',
+        },
+        {
+          title: 'Sách Tứ Thánh Đế - Giọng Đọc - Tâm Như Hạnh',
+          type: mediaType.mixCloud,
+          href:
+            'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fuy%C3%AAn-nguy%E1%BB%85n122%2Fplaylists%2F4thanhde-latoithuong-tam-nhu-hanh%2F',
+          speaker: persons.hy,
+          author: persons.hv,
+          date: '',
+        },
+        {
+          title: 'Sách Tứ Thánh Đế - Giọng Đọc - Thúy Bình',
+          type: mediaType.mixCloud,
+          href:
+            'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fuy%C3%AAn-nguy%E1%BB%85n122%2Fplaylists%2F4thanhde-latoithuong-thuy-binh%2F',
+          speaker: persons.hy,
+          author: persons.hv,
+          date: '',
+        },
+      ],
+    },
+    {
+      categoryName: 'Diệu Pháp Âm - Kinaya Theo Chủ Đề',
+      items: [
+        {
+          title: 'Nikaya - Duyên Thọ',
+          type: mediaType.mixCloud,
+          href:
+            'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fuy%C3%AAn-nguy%E1%BB%85n122%2Fplaylists%2Fduy%C3%AAn-th%E1%BB%8D%2F',
+          speaker: persons.mn,
+          author: persons.hv,
+          date: '',
+        },
+      ],
+    },
+  ];
   return (
     <StaticLayout
       title={title}
@@ -15,27 +183,9 @@ export default function DPA({ page, links }) {
       markdown={markdown}
       links={links}
     >
-      <h2>Dpa - Hòa Võ - Sơ thiện, Trung Thiện, Hậu Thiện</h2>
-      <iframe
-        title="Dpa - Hòa Võ - Sơ thiện, Trung Thiện, Hậu Thiện"
-        src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fhoa.vo.5855594%2Fvideos%2F2763907837201221%2F&show_text=false&width=560"
-        width="560"
-        height="314"
-        style={{ border: 'none', overflow: 'hidden' }}
-        scrolling="no"
-        frameBorder="0"
-        allowFullScreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      />
-      <br />
-      <h2>Dpa 4td - Minh Nguyệt</h2>
-      <iframe
-        title="Dpa 4td - MN"
-        width="100%"
-        height="180"
-        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fuy%C3%AAn-nguy%E1%BB%85n122%2Fplaylists%2Fmn-4thanhde-latoithuong%2F"
-        frameBorder="0"
-      />
+      {data.map((category) => (
+        <MediaCategory {...category} />
+      ))}
     </StaticLayout>
   );
 }
